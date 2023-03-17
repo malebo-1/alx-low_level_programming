1f1d287f0ad8fabb2da9e5027201735d6e61a11c
malebo <malebokomape788@gmail.com>
2023-03-17 14:40:29 -0700

    The last digit of the number stored in the variable n

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <unistd.h>
 
 /**
- * main -Entry point
- * Return: Always 1
+ * main - Entry
+ * Return: always  
  */
-
 int main(void)
 {
-	write(2, ''and that piece of art is useful" - Dora Korpar, 2015-10-19\n'', 59);
+	write(1, ' 'and that piece of art is useful" - Dora Korpar, 2015-10-19\n' ', 59);
 	return (1);
 }
