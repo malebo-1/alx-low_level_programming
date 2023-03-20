4a7d98e954f823c13da2a4c6d86934ff2a4dd14f
malebo <malebokomape788@gmail.com>
2023-03-20 04:11:05 -0700

    a program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints allpossible combs of two two digits'
+ * Return: Always 0
+ */
+int main(void)
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print comb of numbers'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
