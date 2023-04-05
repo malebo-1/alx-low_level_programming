1118d74f29ed4385edda58b95f670371d4547415
malebo <malebokomape788@gmail.com>
2023-04-05 07:19:35 -0700

    prints a string, followed by a new line.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion - like puts ();
+ * _puts_recursion - function like puts();
  * @s: input
  * Return: Always 0 (success)
  */
@@ -14,5 +14,5 @@ void _puts_recursion(char *s)
 	}
 
 	else
-		_putchar('n');
+		_putchar('\n');
 }
