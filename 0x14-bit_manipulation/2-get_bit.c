aa0800e898e26aa4e7ba60e63c85eaacc8f0fce7
root <malebokomape788@gmail.com>
2023-07-06 14:29:18 -0700

    a function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,8 +9,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
-		return (-1);
+	int bit_val;
 
-	if ((n & (1 << index)) == 0)
+	if (index > 63)
+		return (-1);
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
 }
