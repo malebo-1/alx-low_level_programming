507aedba14742e13592a3e3da84749b016c62d23
malebo komape <malebokomape788@gmail.com>
2023-08-21 13:58:02 -0700

    a function that locates a substring.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.c"
+
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: The input
+ * @needle: The input
+ * Return: Always 0 (success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *n = haystack;
+		char *k = needle;
+
+		while (*n == *k && *k != '\0')
+		{
+			n++;
+			k++;
+		}
+
+		if (*k == '\0')
+			return (haystack);
+	}
+
+	return (0)
+}
